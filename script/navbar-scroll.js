dd5c00fb78ea712a7466946fb039dca1d965dc71
mzaedan <zaedanm108@gmail.com>
2023-10-04 07:07:02 +0700

    update

--- script/navbar-scroll.js
+++ script/navbar-scroll.js
@@ -1,6 +1,6 @@
 $(function () {
   $(document).scroll(function () {
     var $nav = $(".navbar-fixed-top");
-    $nav.toggleclass("scrolled", $this.scrollTop() > $nav.height());
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
   });
 });
